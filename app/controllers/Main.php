9d3e478a54a59b2567de7a7a72b76a69f4358f26
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-04 12:48:06 -0400

    updated

---
+++ app/controllers/Main.php
@@ -0,0 +1,44 @@
+<?php
+namespace app\controllers;
+
+class Main extends \app\core\Controller{
+	public function index() {
+		if(isset($_POST['action'])) {
+			$user = new \app\models\User();
+			
+				header('location:/User/index');
+			}else{
+				header('location:/User/index?error=Wrong username/password combination!');
+			}
+		}else{
+			$this->view('User/index');
+		}
+	}
+
+	public function register() {
+		$this->view('User/register');
+
+		if(isset($_POST['action'])) {
+			if($_POST['password'] == $_POST['password_confirm']) {
+				$user = new \app\models\User();
+				$check = $user->get($_POST['username']);
+				if(!$check) {
+					$user->username = $_POST['username'];
+					$user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+					$user->insert();
+					header('location:/User/index');
+				} else {
+					header('location:/User/register?error=The username "'.$_POST['username'].'" is already in use. Select another.');
+				}
+			} else {
+				header('location:/User/register?error=Passwords do not match.');
+			}
+		}		
+	}
+
+	#[\app\filters\User]
+	public function logout(){
+		session_destroy();
+		header('location:/User/index');
+	}
+}
