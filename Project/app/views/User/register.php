4754f50f43a7a3b34d71e8bd28d69609dfb74a68
DHay10 <son-ta@hotmail.fr>
2022-11-24 21:16:42 -0500

    Update register.php

--- Project/app/views/User/register.php
+++ Project/app/views/User/register.php
@@ -11,125 +11,82 @@
     </head>
 
     <body>
-        <main>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-5">
-                        <div class="card">
-                            <h2 class="card-title text-center">Register as User</a></h2>
-                            <hr >
-                                <div class="card-body py-md-4">
-
-                                    <form _lpchecked="1" action='' method='post'>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="username" name="username" placeholder="Username">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="Confirm Password">
-                                        </div>
-                                        
-										<div class="d-flex flex-row align-items-center justify-content-between">
-                                            <a href="\Main\index">Back</a>
-                                            <a href="\User\login">Already a Member?</a>
-                                            <button name="action" type="submit" class="btn btn-primary">Register</button>
-                                        </div>
-                                    </form>
-                            </div>
+        <?php include 'app\views\includes\userHeader.php'; ?>
+        <?php include 'app\views\includes\error.php'; ?>
+        
+        <div class='container mb-4'>
+            <h2 class="h1-responsive font-weight-bold text-center my-4">Register</h2>
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" name="username" placeholder="" required>
+                            <label for="username">Username</label>
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" name="fName" placeholder="" required>
+                            <label for="fname">First Name</label>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" name="lName" placeholder="" required>
+                            <label for="lname">Last Name</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="tel" class="form-control" name="phone" placeholder="" required>
+                            <label for="phone">Phone</label>
+                            <div id="phoneHelp" class="form-text">I.e. (XXX)XXX-XXXX</div>
+                        </div>
+                    </div>
+                    
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" name="email" placeholder="" required>
+                            <label for="email">Email</label>
+                            <div id="emailHelp" class="form-text">example@example.com</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" name="password" placeholder="" minlength="8" required>
+                            <label for="password">Password</label>
+                            <div id="passwordHelp" class="form-text">At least 8 characters</div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" name="password_conf" placeholder="" required>
+                            <label for="passwordConf">Password Confirmation</label>
+                            <div id="passwordConfHelp" class="form-text">Re-enter your Password</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-center text-md-right">
+                    <button name="action" type="submit" class="btn btn-dark w-50">Sign In</button>
+                </div>
+
+                <div class="text-center text-md-right">
+                    <small>Already have an Account? <a href="/User/login">Log In</a></small>
+                </div>
+
+            </form>
+        </div>
     </body>
-
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=PT+Sans');
-
-        body{
-            background: #fff;
-            font-family: 'PT Sans', sans-serif;
-        }
-        h2{
-            padding-top: 1.5rem;
-        }
-        .card{
-            border: 0.40rem solid #f8f9fa;
-            top: 10%;
-        }
-        .form-control{
-            background-color: #f8f9fa;
-            padding: 20px;
-            padding: 25px 15px;
-            margin-bottom: 1.3rem;
-        }
-
-        .form-control:focus {
-            color: #000000;
-            background-color: #ffffff;
-            border: 3px solid #da5767;
-            outline: 0;
-            box-shadow: none;
-        }
-        .btn{
-            padding: 0.6rem 1.2rem;
-            background: #50c2d1;
-            border: 2px solid #50c2d1;
-        }
-        .btn-primary:hover {
-            background-color: #00D100;
-            border-color: #00D100;
-            transition: .3s;
-        }
-
-        /* Style the navigation menu */
-        .navbar {
-            width: 100%;
-            background-color: rgb(255, 255, 255);
-            overflow: auto;
-        }
-
-        /* Navigation links */
-        .navbar a {
-            float: left;
-            padding: 10px;
-            color: black;
-            text-decoration: none;
-            font-size: 17px;
-            width: 50%; /* Four equal-width links. If you have two links, use 50%, and 33.33% for three links, etc.. */
-            text-align: center; /* If you want the text to be centered */
-        }
-
-        /* Add a background color on mouse-over */
-        .navbar a:hover {
-            background-color: rgb(240, 240, 240);
-        }
-
-        /* Style the current/active link */
-        .navbar a.active {
-            background-color: rgb(230, 230, 230);
-        }
-
-        /* Add responsiveness - on screens less than 500px, make the navigation links appear on top of each other, instead of next to each other */
-        @media screen and (max-width: 500px) {
-        .navbar a {
-            float: none;
-            display: block;
-            width: 100%;
-            text-align: left; /* If you want the text to be left-aligned on small screens */
-        }
-        }
-
-    </style>
 </html>
