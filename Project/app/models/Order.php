83519641a229c9afe5b7a27016c1036f16ae23bf
DHay10 <son-ta@hotmail.fr>
2022-12-10 00:59:47 -0500

    Added Order Model

---
+++ Project/app/models/Order.php
@@ -0,0 +1,39 @@
+<?php
+namespace app\models;
+
+class Order extends \app\core\Model {
+
+    public function insert() {
+        $SQL = "INSERT INTO order(user_id, total, date, status, address) VALUES (:user_id, :total, :date, :status, :address)";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['user_id'=>$this->user_id,
+                        'total'=>$this->total,
+                        'date'=>$this->date,
+                        'total'=>$this->status,
+                        'total'=>$this->adress]);
+    }
+
+    public function getByUserID($user_id) {
+        $SQL = "SELECT * FROM order WHERE user_id=:user_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['user_id'=>$user_id]);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Order');
+        return $STMT->fetchAll();
+    }
+
+    public function getAll() {
+        $SQL = "SELECT * FROM order";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute();
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Order');
+        return $STMT->fetchAll();
+    }
+
+    public function updateStatus() {
+        $SQL = "UPDATE product SET status=:status WHERE order_id=:order_id";
+		$STMT = self::$_connection->prepare($SQL);
+		$STMT->execute(['status'=>$this->status,
+                        'order_id'=>$this->order_id]);
+    }
+
+}
---
+++ Project/app/models/Order_Item.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\models;
+
+class Order_Item extends \app\core\Model {
+
+    public function insert() {
+        $SQL = "INSERT INTO order_item(order_id, product_id, unit_price, qty) VALUES (:order_id, :product_id, :unit_price :qty)";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['order_id'=>$this->order_id,
+                        'product_id'=>$this->product_id,
+                        'unit_price'=>$this->unit_price,
+                        'qty'=>$this->qty]);
+    }
+
+    public function getByID($order_item_id) {
+        $SQL = "SELECT * FROM order_item WHERE order_item_id=:order_item_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['order_item_id'=>$order_item_id]);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Order_Item');
+        return $STMT->fetch();
+    }
+
+    public function getAllByOrderID($order_id) {
+        $SQL = "SELECT * FROM order_item WHERE order_id=:order_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['order_id'=>$order_id]);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Order_Item');
+        return $STMT->fetchAll();
+    }
+
+
+
+}
